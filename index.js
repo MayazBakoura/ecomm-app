b3b021e0d54d56ead572a86b01f89dd3aa2d7ad2
Mayaz Bakoura <mayazbacoura@gmail.com>
2026-03-09 05:28:00 +0400

    Added simple local server with html buttons for sign up

---
+++ index.js
@@ -0,0 +1,24 @@
+const express= require('express');
+const app = express();
+
+
+app.get('/',(req,res) =>{
+
+
+  res.send(`
+    <div>
+    <form>
+    <input placeholder="email"/>
+    <input placeholder="password"/>
+    <input placeholder="password confirmation"/>
+<button>Sign up</button>
+
+    </form>
+    </div>
+    
+    `)
+});
+
+app.listen(3000,() =>{
+  console.log("listening")
+})
