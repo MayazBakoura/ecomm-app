9c418485b9290064f1c44f70cfeab1633837922c
Mayaz Bakoura <mayazbacoura@gmail.com>
2026-03-12 00:06:05 +0400

    feat: Added  update func that adds attributes

--- repositories/user.js
+++ repositories/user.js
@@ -48,11 +48,22 @@ const records = await this.getAll();
 const filteredRecords = records.filter(record => record.id !==id)
 await this.writeAll(filteredRecords);
 }
+async update(id ,attrs){
+const records = await this.getAll();
+const record = records.find(record => record.id === id)
+
+if(!record){
+throw new Error(`Record with ${id} not found`);
+}
+Object.assign(record, attrs);
+await this.writeAll(records);
+}
+
 }
 const test = async () =>{
 
 const repo = new UsersRepositories('users.json');
-await repo.delete('8bed766c');
+await repo.create({email:'test@teast.com'});
 
 };
 test();
