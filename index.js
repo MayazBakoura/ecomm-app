080a86b32d6db7c2c97b1f5f9ce3dc36119c0d57
Mayaz Bakoura <mayazbacoura@gmail.com>
2026-03-12 04:58:10 +0400

    feat: Added cookies to identify a user

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express= require('express');
 const app = express();
 const usersRepo = require('./repositories/users');
-
+const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
+
+
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieSession({
+keys:['asd123']
+
+}));
+
+
 app.get('/',(req,res) =>{
 
 
   res.send(`
     <div>
+    Your id is :${req.session.userId}
     <form method="POST">
     <input name="email" placeholder="email"/>
     <input name="password" placeholder="password"/>
@@ -33,6 +43,13 @@ app.post('/', async (req,res) =>{
 if(password!=passwordConfirmation){
    return res.send("Passwords must match")
 }
+
+
+const user = await usersRepo.create({email , password});
+
+// Store the id of that user inside the users cookie
+req.session.userId = user.id;
+
  res.send("account created")
 });
 
--- repositories/users.js
+++ repositories/users.js
@@ -29,7 +29,7 @@ const records = await this.getAll();
 records.push(attrs);
 
 await this.writeAll(records);
-
+return attrs;
 }
 
 async writeAll(records){
