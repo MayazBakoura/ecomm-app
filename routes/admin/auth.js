d4690af672b2ca32308ce3d1d396450f08afc487
Mayaz Bakoura <mayazbacoura@gmail.com>
2026-06-10 21:24:19 +0400

    feat: reuseable ErrorHandling func + bugs fixed

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator');
+const {handleErrors} = require('./middlewares');
 
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
@@ -20,15 +20,11 @@ router.get('/signup', (req, res) => {
 
 router.post(
   '/signup',
-  [requireEmail, requirePassword, requirePasswordConfirmation],
+  [requireEmail, requirePassword, requirePasswordConfirmation],handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
+   
 
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-    }
-
-    const { email, password, passwordConfirmation } = req.body;
+    const { email, password } = req.body;
     const user = await usersRepo.create({ email, password });
 
     req.session.userId = user.id;
@@ -48,13 +44,9 @@ router.get('/signin', (req, res) => {
 
 router.post(
   '/signin',
-  [requireEmailExists, requireValidPasswordForUser],
+  [requireEmailExists, requireValidPasswordForUser],handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors }));
-    }
+    
 
     const { email } = req.body;
 
