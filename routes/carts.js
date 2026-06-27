f11e659f3417918e914a3a647f9b1c92b5de9819
Mayaz Bakoura <mayazbacoura@gmail.com>
2026-06-27 18:15:26 +0400

    feat: added cart page

--- routes/carts.js
+++ routes/carts.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cartsRepo = require('../repositories/carts')
 const router = express.Router();
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
 
 router.post('/cart/products', async (req,res) =>{
   let cart;
@@ -24,7 +26,29 @@ existingItem.quantity++;
 cart.items.push({id: req.body.productId, quantity: 1})
 }
 await cartsRepo.update(cart.id,{items:cart.items} )
-  res.send("Product added");
+ res.redirect('/cart');
 });
 
+router.get('/cart', async (req,res) =>{
+if(!req.session.cartId) {
+
+  return res.redirect('/');
+}
+const cart = await cartsRepo.getOne(req.session.cartId);
+for(let item of cart.items){
+const product = await productsRepo.getOne(item.id);
+item.product = product;
+
+}
+res.send(cartShowTemplate({items: cart.items}));
+});
+
+router.post('/cart/products/delete', async(req,res) =>{
+const {itemId} = req.body;
+const cart = await cartsRepo.getOne(req.session.cartId);
+
+const items = cart.items.filter(item => item.id !== itemId);
+await cartsRepo.update(req.session.cartId, {items});
+res.redirect('/cart');
+});
 module.exports= router;
